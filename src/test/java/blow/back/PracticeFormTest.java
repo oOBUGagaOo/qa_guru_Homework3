7ade9b78746f8327d39a6696b3ebcc0b2d0244d4
ZzDmitryPY <103034855+ZzDmitryPY@users.noreply.github.com>
2023-02-24 17:56:43 +0300

    commit5

--- src/test/java/blow/back/PracticeFormTest.java
+++ src/test/java/blow/back/PracticeFormTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.File;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -23,21 +24,23 @@ public class PracticeFormTest {
   void FillFormTest() {
     $("#firstName").setValue("Dmitry");
     $("#lastName").setValue("Plotnikov");
-
     $("#userEmail").setValue("kokoko@koko.ru");
-
     $("#userNumber").setValue("900800700");
-
     $("#genterWrapper").$(byText("Other")).click();
-
     $("#dateOfBirthInput").click();
     $(".react-datepicker__month-select").$(byText("August")).click();
     $(".react-datepicker__year-select").$(byText("1997")).click();
     $(".react-datepicker__month-container").$(byText("26")).click();
-
     $("#subjectsInput").setValue("English").pressEnter();
-
     $(byText("Music")).click();
+    $("#currentAddress").setValue("SDFSDFS");
+    $("#state").click();
+    $("#stateCity-wrapper").$(byText("NCR")).click();
+    $("#city").click();
+    $("#stateCity-wrapper").$(byText("Delhi")).click();
+    $("#submit").click();
+
+    $(".modal-header").shouldHave(text("Thanks for submitting the form"));
 
 
 
