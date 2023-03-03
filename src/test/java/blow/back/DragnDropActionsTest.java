4074d5aac8d93cf464b2151b01d8caf843c3baa0
Dmitry Plotnikov <103034855+oOBUGagaOo@users.noreply.github.com>
2023-03-03 16:29:05 +0300

    modification class DragnDropActionsTest2

--- src/test/java/blow/back/DragnDropActionsTest.java
+++ src/test/java/blow/back/DragnDropActionsTest.java
@@ -22,6 +22,7 @@ public class DragnDropActionsTest {
             .clickAndHold(sourceEl)
             .moveToElement(targetEl)
             .release()
+            .build()
             .perform();
   }
 }
